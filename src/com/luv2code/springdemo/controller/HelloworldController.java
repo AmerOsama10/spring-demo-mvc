94b9ec505ca805b7fe3376162f177117e782c5b3
amer <amerosama.jr@gmail.com>
2022-12-23 07:09:06 +0200

    @Request mapping

--- src/com/luv2code/springdemo/controller/HelloworldController.java
+++ src/com/luv2code/springdemo/controller/HelloworldController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("")
 public class HelloworldController {
 
 	// need a controller method to show the default or home page.
---
+++ src/com/luv2code/springdemo/controller/SillyController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/silly")
+public class SillyController {
+
+	// need a controller method to show the initial HTML form.
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "show-form";
+	}
+	
+
+	@RequestMapping("/processFormV3")
+	public String processFormVersionThree(@RequestParam("studentName") String studentName,@RequestParam("studentPassword") String studentPassword, Model model) {
+
+
+		// convert the data to upper case 
+		// Do your logic
+		studentName = studentName.toUpperCase();
+		studentPassword = studentPassword.toUpperCase();
+
+		int len = studentPassword.length();
+		System.out.println(len);
+		// add the message to the model
+		model.addAttribute("studentName", studentName);
+		model.addAttribute("studentPassword", studentPassword);
+		model.addAttribute("len", len);
+
+		return "process-data-v3";
+	}
+}
